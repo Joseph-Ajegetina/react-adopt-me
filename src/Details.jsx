ab2a3566dbb8880acea51b380e51c85c0d5c2063
Joseph-Ajegetina <josephajongbah@gmail.com>
2023-09-24 15:03:32 +0000

    portal refs for a modal and context

--- src/Details.jsx
+++ src/Details.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
 import ThemeContext from "./ThemeContext";
-
+import Modal from "./Modal";
 class Details extends Component {
   //Without babel class properties plugin
   // constructor() {
@@ -11,7 +11,8 @@ class Details extends Component {
   //   this.state = { loading: true };
   // }
 
-  state = { loading: true };
+  state = { loading: true, showModal: false };
+  toggleModal = () => this.setState({ showModal: !this.state.showModal });
 
   async componentDidMount() {
     const res = await fetch(
@@ -28,7 +29,7 @@ class Details extends Component {
       return <h2>loading … </h2>;
     }
 
-    const { animal, breed, city, state, description, name, images } =
+    const { animal, breed, city, state, description, name, images, showModal } =
       this.state;
 
     return (
@@ -39,10 +40,26 @@ class Details extends Component {
           <h2>{`${animal} — ${breed} — ${city}, ${state}`}</h2>
           <ThemeContext.Consumer>
             {([theme]) => (
-              <button style={{ backgroundColor: theme }}>Adopt {name}</button>
+              <button
+                onClick={this.toggleModal}
+                style={{ backgroundColor: theme }}
+              >
+                Adopt {name}
+              </button>
             )}
           </ThemeContext.Consumer>
-          ;<p>{description}</p>
+          <p>{description}</p>
+          {showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {name}?</h1>
+                <div className="buttons">
+                  <a href="https://bit.ly/pet-adopt">Yes</a>
+                  <button onClick={this.toggleModal}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
         </div>
       </div>
     );
